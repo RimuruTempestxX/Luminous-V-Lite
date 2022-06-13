e3a870eb7a6fe528f4d071e0386355ea493ce9b6
RimuruTempestxX <a79436328@gmail.com>
2022-06-13 18:43:59 +0800

    V LITE

---
+++ admin/iklan.js
@@ -0,0 +1,11 @@
+exports.iklan1 = () => {
+  return `[ Bot Pertama wa.me/6282253479547 ]`
+};
+
+exports.iklan2 = () => {
+  return `[ Silahkan Join https://chat.whatsapp.com/CXh8962GOxK6cwDwLjlABw ]`
+};
+
+exports.iklan3 = () => {
+  return `[ none ]`
+};
